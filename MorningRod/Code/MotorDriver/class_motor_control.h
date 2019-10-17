22febad7dd28d4f3d7a14dbf063fec9454fc6fa0
Robert Kimmell <kimmellr@gmail.com>
2019-10-16 21:11:00 -0700

    Fix types, code now compiles without error

--- MorningRod/Code/MotorDriver/class_motor_control.h
+++ MorningRod/Code/MotorDriver/class_motor_control.h
@@ -1,15 +1,27 @@
 #ifndef Motor_control
 #define Motor_control
-// #include "Arduino.h"
+ #include "Arduino.h"
+
+
+
+
 class Movement
 {
-  unsigned long _current;
-  long _velocity;
-  long _travel;
-  long _stallguard;
-  
+    unsigned long _current;
+    long _velocity;
+    long _travel;
+    long _stallguard;
+    int _chipCS;
+    const static int SCLK = 26;
+    const static int MISO = 33;
+    const static int MOSI = 25;
+
+    const static int CLOCKOUT = 15;
+    const static int ENABLE_PIN = 32;
+
   public :
-    Movement(unsigned long current, long velocity, long travel, long stallguard);
+    Movement(unsigned long current, long velocity, long travel, long stallguard, int chipCS);
+    unsigned long sendData(unsigned long address, unsigned long datagram);
     void move();
 };
 
@@ -27,21 +39,27 @@ class MotorControl
     bool _finding_home;         // Used to set absolute position using move_home
     bool _full_open;            // am i running all the way open
     bool _full_close;           // am i running all the way close
+    const static int SCLK = 26;
+    const static int MISO = 33;
+    const static int MOSI = 25;
+
+    const static int CLOCKOUT = 15;
+    const static int ENABLE_PIN = 32;
 
   public:
-  // Motor_control(int stallguard, long travel, long velocity);
+    // Motor_control(int stallguard, long travel, long velocity);
     MotorControl();
     void Begin(int stallguard, long travel, long velocity, long current_open, long current_close);
-    void setup_pins(int chipCS, int CLOCKOUT, int ENABLE_PIN);
+    void setup_pins(int chipCS);
     void move_open();
     void move_close();
     void move_position(float position);
     void move_stop();
     void move_home();
-    int get_position():
+    //    int get_position():
     void check_stall();
     void setup_motors();
-    unsigned long sendData(nsigned long address, unsigned long datagram);
+    unsigned long sendData(unsigned long address, unsigned long datagram);
 
 
 };
